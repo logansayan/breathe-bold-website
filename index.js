82b5f7849900e5ad9d1abbec06ef74c1c718857d
logansayan <dassayan375@gmail.com>
2022-06-02 20:03:51 +0530

    changed about section

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 $('.navbar__toggle').click(function () {
     $('.navbar__links').toggleClass('navbar__remove')
 })
+
+$('.plan-info__items').hide()
+
+$('.plan-info__toggle').click(function () {
+    $('.plan-info__toggle').hide()
+    $('.plan-info__items').addClass('fade-in').slideDown(300)
+})
