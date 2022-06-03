26c84cb96f8db10f956bbb2daad019cd997613f0
logansayan <dassayan375@gmail.com>
2022-06-03 12:12:42 +0530

    add popup to try free button

--- index.js
+++ index.js
@@ -2,6 +2,16 @@ $('.navbar__toggle').click(function () {
     $('.navbar__links').toggleClass('navbar__remove')
 })
 
+$('.header__popup').hide()
+
+$('.header-btn__try').click(function () {
+    $('.header__popup').fadeIn()
+})
+
+$('.header__popup--cancel').click(function() {
+    $('.header__popup').fadeOut()
+})
+
 $('.plan-info__items').hide()
 
 $('.plan-info__toggle').click(function () {
