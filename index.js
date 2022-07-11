a666c1e9550e126a71c46b1bd0bc257aeef8e8e9
logansayan <dassayan375@gmail.com>
2022-07-11 14:16:32 +0530

    navbar fixed and top button add

--- index.js
+++ index.js
@@ -1,20 +1,56 @@
-$('.navbar__toggle').click(function () {
-    $('.navbar__links').toggleClass('navbar__remove')
-})
+$(document).ready(() => {
 
-$('.header__popup').hide()
+        // GO TO TOP BUTTON
+        let topBtn = $(".go-top")
+        topBtn.hide()
 
-$('.header-btn__try').click(function () {
-    $('.header__popup').fadeIn()
-})
+        topBtn.click(() => {
+            topBtn.fadeOut()
+        })
 
-$('.header__popup--cancel').click(function() {
-    $('.header__popup').fadeOut()
-})
+        // NAVBAR HIDE ON SCROLL
+        const nav = $('.navbar')
+        let lastScrollY = window.scrollY
+    
+        window.addEventListener("scroll", () => {
+            if (lastScrollY < window.scrollY) {
+                nav.slideUp(200)
 
-$('.plan-info__items').hide()
+                // Handle Navbar links
+                $('.navbar__links').addClass('navbar__remove')
 
-$('.plan-info__toggle').click(function () {
-    $('.plan-info__toggle').hide()
-    $('.plan-info__items').addClass('fade-in').slideDown(300)
+                // Handle Top button
+                topBtn.fadeIn()
+            } else {
+                nav.slideDown(200)
+            }
+            lastScrollY = window.scrollY
+        })
+    
+    
+        // NAVBAR TOGGLE BUTTON
+        $('.navbar__toggle').click(function () {
+            $('.navbar__links').toggleClass('navbar__remove')
+        })
+    
+    
+        // OPEN TRY NOW BUTTON
+        $('.header__popup').hide()
+    
+        $('.header-btn__try').click(function () {
+            $('.header__popup').fadeIn()
+        })
+    
+        // CLOSE TRY NOW BUTTON
+        $('.header__popup--cancel').click(function() {
+            $('.header__popup').fadeOut()
+        })
+    
+        // PLAN INFO BUTTON
+        $('.plan-info__items').hide()
+    
+        $('.plan-info__toggle').click(function () {
+            $('.plan-info__toggle').hide()
+            $('.plan-info__items').addClass('fade-in').slideDown(300)
+        })
 })
